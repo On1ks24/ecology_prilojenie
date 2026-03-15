b18a22ebcdaaffa624557ebc452010e1724820ac
On1ks24 <saraev200517@gmail.com>
2026-03-15 22:18:49 +0700

    теперь запускается
    вроде

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,3 +1,16 @@
-const { getDefaultConfig } = require('metro-config');
+// metro.config.js
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  watchFolders: [path.resolve(__dirname, './')],
+  resolver: {
+    // Дополнительные настройки для резолвинга модулей (если нужно)
+    // Например, если у вас есть симлинки или монорепозиторий
+    // extraNodeModules: new Proxy({}, { get: (_, name) => path.join(__dirname, `node_modules/${name}`) }),
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
