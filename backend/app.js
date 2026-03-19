a649c80b1c86d767c71e469067fcae67a3ac4efc
On1ks24 <saraev200517@gmail.com>
2026-03-19 17:40:40 +0700

    РАБОЧИЙ ПУСТОЙ ПРОЕКТ С БЭКОМ И ФРОНТОМ
    ФРОНТ ПЕРЕИМЕНОВАН В TEMPAPP
    
    чтоб запустилось добавь переменную среды ANВROID_HOME И там путь к sdk
    
    еще в /tempapp/android/local.properties замени юзера

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
